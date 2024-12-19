a446703dc60d4e835f57fef9fd6922cf4a7fdce2
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2024-12-19 02:16:26 +0100

    Dummy offers

--- barber-panel/src/components/reservation/Offers.tsx
+++ barber-panel/src/components/reservation/Offers.tsx
@@ -13,6 +13,69 @@ const offers = [
         price: 120,
         time: 90,
         description: 'Farbowanie włosów z możliwością konsultacji koloru.',
+    },
+    {
+        id: '3',
+        label: 'Strzyżenie damskie',
+        price: 60,
+        time: 45,
+    },
+    {
+        id: '4',
+        label: 'Modelowanie',
+        price: 50,
+        time: 30,
+    },
+    {
+        id: '5',
+        label: 'Pielęgnacja brody',
+        price: 30,
+        time: 20,
+    },
+    {
+        id: '6',
+        label: 'Prostowanie keratynowe',
+        price: 200,
+        time: 120,
+        description: 'Zabieg prostowania włosów z użyciem keratyny.',
+    },
+    {
+        id: '7',
+        label: 'Strzyżenie dziecięce',
+        price: 35,
+        time: 20,
+    },
+    {
+        id: '8',
+        label: 'Trwała ondulacja',
+        price: 150,
+        time: 100,
+        description: 'Zabieg trwałej ondulacji włosów.',
+    },
+    {
+        id: '9',
+        label: 'Regeneracja włosów',
+        price: 80,
+        time: 60,
+        description: 'Zabieg regenerujący włosy.',
+    },
+    {
+        id: '10',
+        label: 'Strzyżenie maszynką',
+        price: 25,
+        time: 15,
+    },
+    {
+        id: '11',
+        label: 'Farbowanie brody',
+        price: 40,
+        time: 30,
+    },
+    {
+        id: '12',
+        label: 'Stylizacja brwi',
+        price: 20,
+        time: 15,
     }
 ];
 
