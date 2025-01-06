c53045e6ecbc3969bfd0f8e886e3f0de8b385622
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-07 00:16:11 +0100

    Removed unused import

--- barber-panel/src/components/landing/Hero.tsx
+++ barber-panel/src/components/landing/Hero.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import { motion, useScroll, useTransform, useAnimation } from 'framer-motion';
 import HeroVideo from '../../assets/hero_vid.mp4';
 import SecondVideo from '../../assets/second_vid.mp4';
