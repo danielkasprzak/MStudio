a2ab689748b060d1f48ba5daa65516b63e712a00
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-09 02:16:02 +0100

    Text vertically centered with another element

--- barber-panel/src/components/landing/Slogan.tsx
+++ barber-panel/src/components/landing/Slogan.tsx
@@ -5,12 +5,12 @@ interface Props {
 }
 
 export default ({ sloganAnimation }: Props) => {
-    const texts = ["STYL", "PIĘKNO", "SIEBIE"];
+    const texts = ["PIĘKNO", "STYL", "SIEBIE"];
 
     return (
         <motion.div className='pt-[14%] flex flex-row justify-center items-center w-full h-auto -z-10 text-10xl font-cormorant font-medium text-white'>
-            <h1 className='p-4'>ODKRYJ</h1>
-            <motion.div className='h-60 p-4 overflow-y-hidden flex flex-col'>
+            <h1 className='p-4 h-60'>ODKRYJ</h1>
+            <motion.div className='p-4 overflow-hidden flex flex-col h-60'>
                 {texts.map((text, index) => (
                     <motion.span key={index} className='italic pb-16' animate={sloganAnimation} transition={{ duration: 0.2 }}>
                         {text}
