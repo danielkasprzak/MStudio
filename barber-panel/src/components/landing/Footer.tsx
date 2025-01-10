6131621bbc626eec0fc3733a206db5570eecb3d0
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 02:42:47 +0100

    Added booking button

--- barber-panel/src/components/landing/Footer.tsx
+++ barber-panel/src/components/landing/Footer.tsx
@@ -3,15 +3,14 @@ import SmallAnchor from "./SmallAnchor";
 export default () => {
 
     return (  
-        <footer className="relative w-full h-auto bg-white text-charcoal font-lato flex flex-row items-center justify-center p-16">
+        <footer className="relative w-full h-auto bg-white text-charcoal font-lato flex flex-row items-center justify-center p-16 z-10">
             <h3 className="font-cormorant font-medium text-xl p-16">MSTUDIO</h3>
-
             <div className="flex flex-col justify-center p-16">
                 <SmallAnchor>Wstęp</SmallAnchor>
                 <SmallAnchor>Poznaj nas</SmallAnchor>
                 <SmallAnchor>Oferta</SmallAnchor>
+                <SmallAnchor>Rezerwacja</SmallAnchor>
             </div>
-
             <div className="flex flex-col justify-center p-16">
                 <SmallAnchor>Facebook</SmallAnchor>
                 <SmallAnchor>Instagram</SmallAnchor>
