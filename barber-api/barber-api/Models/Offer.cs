86ae5a198a7ea93f684cc7ce4623f1a47766308b
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 14:04:58 +0100

    Offers model

---
+++ barber-api/barber-api/Models/Offer.cs
@@ -0,0 +1,10 @@
+﻿namespace barber_api.Models
+{
+    public class Offer
+    {
+        public string Label { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
