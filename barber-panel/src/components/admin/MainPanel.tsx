a3f2e8349f0cc48cf1d52d2c7f4e4f0ebb89849d
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 21:13:39 +0100

    MainPanel router outlet

--- barber-panel/src/components/admin/MainPanel.tsx
+++ barber-panel/src/components/admin/MainPanel.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom"
 export default () => {
     return (
         <div className='relative w-full h-full flex flex-col justify-start'>
-            {/* <Outlet /> */}
+            <Outlet />
         </div>
     )
 }
