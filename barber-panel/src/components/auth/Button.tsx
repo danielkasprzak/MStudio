d5095c05d0adc3fd6a404c6e95084d2b21619d15
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 18:31:10 +0100

    onClick prop

--- barber-panel/src/components/auth/Button.tsx
+++ barber-panel/src/components/auth/Button.tsx
@@ -1,7 +1,8 @@
-interface ButtonProps {
+interface Props {
     children: React.ReactNode;
+    onClick: () => void;
 }
 
-export default ({ children } : ButtonProps) => {
-    return <button className="py-3 m-4 w-80 rounded-3xl border-neutral-900 border text-white text-sm font-montserrat tracking-wider font-light">{children}</button>
+export default ({ children, onClick } : Props) => {
+    return <button onClick={onClick} className="py-3 m-4 w-80 rounded-3xl border-neutral-900 border text-white text-sm font-montserrat tracking-wider font-light">{children}</button>;
 }
