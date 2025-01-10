767a07fadf8e405daf75a923cef1e27d4d7417f4
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 20:03:30 +0100

    No primary key for opening hours

--- barber-api/barber-api/Data/AppDbContext.cs
+++ barber-api/barber-api/Data/AppDbContext.cs
@@ -9,6 +9,10 @@ public class AppDbContext : DbContext
 
     public DbSet<Reservation> Reservations { get; set; }
     public DbSet<Offer> Offers { get; set; }
-    public DbSet<OpeningHour> OpeningHours { get; set; }    
-    
+    public DbSet<OpeningHour> OpeningHours { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OpeningHour>().HasNoKey();
+    }
 }
