ad49fc7ca4eae1069a5476daeb827ba7cff4453e
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 21:35:40 +0100

    New buttons

--- barber-panel/src/components/admin/SidePanel.tsx
+++ barber-panel/src/components/admin/SidePanel.tsx
@@ -3,12 +3,18 @@ import Title from "./Title";
 
 export default () => {
     return (
-        <div className='sticky top-0 h-screen bg-white w-auto flex flex-col text-charcoal'>
-            <Title padding='16'>MSTUDIO</Title>
+        <div className='sticky top-0 h-screen bg-white w-auto flex flex-col justify-between text-charcoal'>
+            <div className="flex flex-col">
+                <Title padding='16'>MSTUDIO</Title>
 
-            <SmallButton>Panel</SmallButton>
-            <SmallButton>Rezerwacje</SmallButton>
-            <SmallButton>Oferta</SmallButton>
+                <SmallButton>Panel</SmallButton>
+                <SmallButton>Rezerwacje</SmallButton>
+                <SmallButton>Oferta</SmallButton>
+            </div>
+            <div className="flex flex-col pb-8">
+                <SmallButton>Powrót</SmallButton>
+                <SmallButton>Wyloguj się</SmallButton>
+            </div>
         </div>
     )
 }
