433421c054eaa65af080877baaea9d30831ebfc7
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 14:03:13 +0100

    OpeningHours model

---
+++ barber-api/barber-api/Models/OpeningHour.cs
@@ -0,0 +1,10 @@
+﻿namespace barber_api.Models
+{
+    public class OpeningHour
+    {
+        public string DayOfWeek { get; set; }
+        public bool IsOpen { get; set; }
+        public DateTime OpenHour { get; set; }
+        public DateTime CloseHour { get; set; }
+    }
+}
