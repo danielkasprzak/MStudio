ec55a13db9961e7a04a26cf416ad6f2544443e64
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 13:59:55 +0100

    Reservation model

---
+++ barber-api/barber-api/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace barber_api.Models
+{
+    public class Reservation
+    {
+        public string ReservationId { get; set; }
+        public string Email { get; set; }
+        public string Services { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReservationDateTime { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
