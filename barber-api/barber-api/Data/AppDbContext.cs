1306d66aac40fb4421a21b33bdf72e1047afee25
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-10 14:07:18 +0100

    Db queries for new models
    - offers
    - opening hours
    - reservations

--- barber-api/barber-api/Data/AppDbContext.cs
+++ barber-api/barber-api/Data/AppDbContext.cs
@@ -1,3 +1,4 @@
+using barber_api.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
@@ -5,4 +6,9 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Offer> Offers { get; set; }
+    public DbSet<OpeningHour> OpeningHours { get; set; }    
+    
 }
