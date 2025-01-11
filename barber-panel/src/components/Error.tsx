7e2be81c651e01ce55afcafd8662daa1d3c17d46
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 15:25:06 +0100

    Custom error providing

--- barber-panel/src/components/Error.tsx
+++ barber-panel/src/components/Error.tsx
@@ -1,3 +1,29 @@
+import { useRouteError } from "react-router-dom"
+
 export default () => {
-    
+    const error = useRouteError();
+
+    let title = "Wystąpił błąd!";
+    let message = "Coś poszło nie tak!"
+
+    if (error instanceof Response && error.status === 500) {
+        error.json().then(data => {
+            message = data.message;
+        });
+    }
+
+    if (error instanceof Response && error.status === 404) {
+        title = "Nie znaleziono!";
+        message = "Strona nie istnieje!";
+    }
+
+    return (
+        <div className="h-full w-full bg-slate-200 flex justify-center items-center">
+            <div className="bg-white p-8 border w-auto h-auto text-charcoal">
+                <h1 className="font-cormorant text-xl font-medium uppercase">{title}</h1>
+                <p className="font-lato uppercase font-bold text-xs tracking-wider">{message}</p>
+
+            </div>
+        </div>
+    );
 }
