9cd631e88c050be081ec0660ef46a6ab1266cbd2
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 14:51:17 +0100

    Constants util file

---
+++ barber-panel/util/constants.ts
@@ -0,0 +1,9 @@
+export const dayTranslations: { [key: string]: string } = {
+    Monday: 'Poniedziałek',
+    Tuesday: 'Wtorek',
+    Wednesday: 'Środa',
+    Thursday: 'Czwartek',
+    Friday: 'Piątek',
+    Saturday: 'Sobota',
+    Sunday: 'Niedziela',
+};
