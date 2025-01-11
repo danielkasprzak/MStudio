3fb7a566aa2d374987d56e980645e95e1a81b352
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 17:57:24 +0100

    Button type

--- barber-panel/src/components/admin/SmallButton.tsx
+++ barber-panel/src/components/admin/SmallButton.tsx
@@ -1,10 +1,11 @@
 interface Props { 
     children?: React.ReactNode;
     onClick?: () => void;
+    type?: "button" | "submit" | "reset";
 }
 
-export default ({children, onClick}: Props) => {
+export default ({children, onClick, type}: Props) => {
     return (
-        <button onClick={onClick} className="uppercase font-bold text-xs tracking-wider p-4">{children}</button>
+        <button onClick={onClick} type={type} className="uppercase font-bold text-xs tracking-wider p-4">{children}</button>
     )
 }
