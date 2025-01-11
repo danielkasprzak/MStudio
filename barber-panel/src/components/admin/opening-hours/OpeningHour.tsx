1d96ba749f89819012ad0867b37cdbeaee0c6f5b
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 23:57:07 +0100

    OpeningHours in admin panel

---
+++ barber-panel/src/components/admin/opening-hours/OpeningHour.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    date: string;
+    isOpen: boolean;
+    openHour: string;
+    closeHour: string;
+}
+
+export default ({ date, isOpen, openHour, closeHour } : Props) => {
+    return (
+        <li className="border border-stone-200 p-8 m-4 w-full h-auto flex flex-row items-center justify-between">
+            <p className="font-bold text-xs tracking-wider">{ date }</p>
+            <p className="font-bold text-xs tracking-wider">{ isOpen ? `${openHour} - ${closeHour}` : 'Zamknięte' }</p>
+        </li>
+    )
+}
---
+++ barber-panel/src/components/admin/opening-hours/OpeningHours.tsx
@@ -0,0 +1,60 @@
+import { useQuery } from '@tanstack/react-query';
+import { queryClient, fetchOpeningHours } from '../../../../util/http';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+
+import Title from '../Title';
+import SmallButton from '../SmallButton';
+import OpeningHour from './OpeningHour';
+
+interface OpeningHoursModel {
+    dayOfWeek: string;
+    isOpen: boolean;
+    openHour: string;
+    closeHour: string;
+}
+
+export default () => {
+    const navigate = useNavigate();
+
+    const { data = [], error } = useQuery<OpeningHoursModel[]>({
+        queryKey: ['openingHours'],
+        queryFn: fetchOpeningHours
+    });
+
+    if (error) return <div>Error loading offers</div>;
+
+    return (
+        <div className='flex flex-row justify-center'>
+            <div className='relative w-fit h-full bg-white m-16 mr-8 text-charcoal font-lato p-8'>
+                <Title padding='8'>Harmonogram otwarcia</Title>
+            
+                <div className='relative w-auto p-16 pt-8 h-full text-charcoal font-lato'>
+                    <ul className='h-fit w-fit'>
+                        {data.map((date) => (
+                            <div className='flex flex-row'>
+                                <OpeningHour key={date.dayOfWeek} date={date.dayOfWeek} isOpen={date.isOpen} openHour={date.openHour} closeHour={date.closeHour} />
+
+                                <div className='flex flex-row'>
+                                    <SmallButton>
+                                        <Link to={`${date.dayOfWeek}`}>Edytuj</Link>
+                                    </SmallButton>
+                                </div>
+                            </div>
+                        ))}
+                        {/* <SmallButton><Link to={`nowa`}>Dodaj</Link></SmallButton> */}
+                    </ul>
+                </div>
+            </div>
+        
+            <Outlet />
+        </div>
+
+    )
+}
+
+export function loader() {
+    return queryClient.fetchQuery({
+        queryKey: ['openingHours'],
+        queryFn: fetchOpeningHours
+    });
+}
