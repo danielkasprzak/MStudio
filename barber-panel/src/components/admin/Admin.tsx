f5529ae84239542a4066751be445a104eeaf6455
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 16:26:58 +0100

    Background color

--- barber-panel/src/components/admin/Admin.tsx
+++ barber-panel/src/components/admin/Admin.tsx
@@ -3,7 +3,7 @@ import SidePanel from "./SidePanel"
 
 export default () => {
     return (
-        <div className="bg-slate-200 h-full w-full flex flex-row justify-center">
+        <div className="bg-stone-100 h-full w-full flex flex-row justify-center">
             <SidePanel />
             <MainPanel />
         </div>
