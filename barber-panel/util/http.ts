5c89801e9accfe9883a4a69139c2793cc082e566
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 15:02:14 +0100

    queryClient to util file

--- barber-panel/util/http.ts
+++ barber-panel/util/http.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
+import { QueryClient } from '@tanstack/react-query';
 
 import { dayTranslations } from "./constants";
+
+export const queryClient = new QueryClient();
 
 export async function fetchOffers() {
     const { data } = await axios.get('https://localhost:7190/offers');
