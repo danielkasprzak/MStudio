388dbe767501f01de50b4e19a1668a0743bfb3c8
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-11 15:33:35 +0100

    Background change

--- barber-panel/src/components/Error.tsx
+++ barber-panel/src/components/Error.tsx
@@ -17,7 +17,7 @@ export default () => {
     }
 
     return (
-        <div className="h-screen w-screen bg-slate-200 flex justify-center items-center">
+        <div className="h-screen w-screen bg-stone-100 flex justify-center items-center">
             <div className="bg-white px-32 py-24 border border-stone-300 w-auto h-auto text-charcoal flex flex-col justify-center items-center">
                 <h1 className="font-cormorant text-xl font-medium uppercase p-4">{title}</h1>
                 <p className="font-lato uppercase font-bold text-xs tracking-wider p-4">{message}</p>
