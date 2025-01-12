7bf98a0af3dac3282898abf1c6f20d755e105d9f
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-12 21:50:10 +0100

    Changed CORS policy

--- barber-api/barber-api/Program.cs
+++ barber-api/barber-api/Program.cs
@@ -11,8 +11,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp", policy =>
     {
         policy.WithOrigins("http://localhost:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
+            .WithHeaders("Content-Type")
+            .WithMethods("GET", "POST", "PUT", "DELETE")
             .AllowCredentials();
     });
 });
