bff6d1e8f5b167daeb5eafdee53f5cb86dfd8ebc
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-15 19:36:44 +0100

    Underfooter animation

--- barber-web/src/components/landing/UnderFooter.tsx
+++ barber-web/src/components/landing/UnderFooter.tsx
@@ -1,12 +1,24 @@
+import { motion, useScroll, useTransform } from 'motion/react';
 import Image from '../../assets/bg_img.jpg';
 import Button from './Button';
+import { useRef } from 'react';
 
 export default () => {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end start"]
+    });
+
+    const y = useTransform(scrollYProgress, [0, 1], ["-20%", "10%"]);
 
     return (  
-        <div className="relative w-full h-auto z-0">
-            <img src={Image} className='absolute w-full h-full inset-0 object-cover'></img>
-            <div className='w-full h-full flex flex-col items-center justify-center p-60'>
+        <div ref={ref} className="relative w-full h-auto z-0">
+            <motion.div className='absolute w-full h-[120%]' style={{top: y}}>
+                <img src={Image} className='absolute w-full h-full inset-0 object-cover'></img>
+            </motion.div>
+            <div className='w-full h-full flex flex-col items-center justify-center p-48'>
+                <h2 className='font-lato text-white font-bold text-xs tracking-wider uppercase z-20'>Nie zwlekaj</h2>
                 <h1 className='font-cormorant font-medium text-3xl text-white uppercase p-4 z-20'>Podążaj za trendami</h1>
                 <Button>Zarezerwuj wizytę</Button>
             </div>
