c460b098d57934f60fc39ab1a2c553685ac47bf5
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-15 19:32:44 +0100

    Landing-page button styling

---
+++ barber-web/src/components/landing/Button.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    children?: React.ReactNode;
+    onClick?: () => void;
+}
+
+
+export default ({ children, onClick } : Props) => {
+    return (
+        <button className="bg-charcoal/20 font-lato text-white text-xs tracking-wider uppercase px-4 py-2 z-30" onClick={onClick}>{children}</button>
+    );
+}
--- barber-web/src/components/landing/UnderFooter.tsx
+++ barber-web/src/components/landing/UnderFooter.tsx
@@ -1,12 +1,14 @@
 import Image from '../../assets/bg_img.jpg';
+import Button from './Button';
 
 export default () => {
 
     return (  
         <div className="relative w-full h-auto z-0">
             <img src={Image} className='absolute w-full h-full inset-0 object-cover'></img>
-            <div className='w-full h-full flex items-center justify-center p-60'>
-                <h1 className='font-cormorant font-semibold text-3xl text-white p-4 z-20'>Podążaj za trendami</h1>
+            <div className='w-full h-full flex flex-col items-center justify-center p-60'>
+                <h1 className='font-cormorant font-medium text-3xl text-white uppercase p-4 z-20'>Podążaj za trendami</h1>
+                <Button>Zarezerwuj wizytę</Button>
             </div>
         </div>
     );
