70c957e071a18ac72e94f41d4669977ee482b742
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-15 02:51:29 +0100

    Fixing link buttons

--- barber-web/src/components/admin/SidePanel.tsx
+++ barber-web/src/components/admin/SidePanel.tsx
@@ -24,17 +24,24 @@ export default () => {
 
     return (
         <div className='sticky top-0 h-screen bg-white border-r border-stone-300 w-auto flex flex-col justify-between text-charcoal'>
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center">
                 <Title padding='16'>MSTUDIO</Title>
 
                 <TextButton>Panel</TextButton>
-                <TextButton><Link to={`rezerwacje`}>Rezerwacje</Link></TextButton>
-                <TextButton><Link to={`oferty`}>Oferta</Link></TextButton>
-                <TextButton><Link to={`godziny-otwarcia`}>Harmonogram</Link></TextButton>
-                <TextButton><Link to={`specjalne-godziny-otwarcia`}>Specjalne</Link></TextButton>
+                <Link to='rezerwacje'>
+                    <TextButton>Rezerwacje</TextButton>
+                </Link>
+                <Link to='oferty'>
+                    <TextButton>Oferta</TextButton>
+                </Link>
+                <Link to='godziny-otwarcia'>
+                    <TextButton>Harmonogram</TextButton>
+                </Link>
+                <Link to='specjalne-godziny-otwarcia'>
+                    <TextButton>Specjalne</TextButton>
+                </Link>
             </div>
             <div className="flex flex-col pb-8">
-                <TextButton>Powrót</TextButton>
                 {isPending ? <div>Wylogowuje...</div> : <TextButton onClick={handleLogout}>Wyloguj się</TextButton>}
             </div>
         </div>
--- barber-web/src/components/admin/special-opening-hours/SpecialOpeningHour.tsx
+++ barber-web/src/components/admin/special-opening-hours/SpecialOpeningHour.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export default ({ date, endDate, isOpen, openHour, closeHour } : Props) => {
     return (
-        <li className="border border-stone-200 p-8 m-4 w-full h-auto flex flex-row items-center justify-between">
+        <li className="border border-stone-300 p-8 m-4 w-full h-auto flex flex-row items-center justify-between">
             <p className="font-bold text-xs tracking-wider pr-4">{endDate ? `${formatDateNoTime(date)} - ${formatDateNoTime(endDate)}` : formatDateNoTime(date) }</p>
             <p className="font-bold text-xs tracking-wider">{ isOpen ? `${openHour} - ${closeHour}` : 'Zamknięte' }</p>
         </li>
