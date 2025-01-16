fd1358b8be033a1a03356b6392dcd0c0f491c3ba
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-16 02:37:18 +0100

    Phone call href

--- barber-web/src/components/reservation/info/Phone.tsx
+++ barber-web/src/components/reservation/info/Phone.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default ({ children } : Props) => {
     return (
-    <div className="mt-8 mb-4 w-full border border-stone-300 px-4 py-2 text-charcoal uppercase font-bold text-xs tracking-wider font-lato flex flex-row justify-center items-center">
-        <div className="border-r border-stone-300 w-auto h-4 mr-3 flex justify-center items-center pr-3"><Phone color="#353535" size={16} strokeWidth={1.25} /></div>
-        {children}
-    </div>
+        <a href={`tel:${children}`} className="mt-8 mb-4 w-full border border-stone-300 px-4 py-2 text-charcoal uppercase font-bold text-xs tracking-wider font-lato flex flex-row justify-center items-center hover:cursor-pointer">
+            <div className="border-r border-stone-300 w-auto h-4 mr-3 flex justify-center items-center pr-3"><Phone color="#353535" size={16} strokeWidth={1.25} /></div>
+            {children}
+        </a>
     );
 }
