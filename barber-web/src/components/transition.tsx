b4f7110087241695f05ff79246aa927eab512ed5
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-17 06:29:04 +0100

    Added temporary transitions for React Router
    This needs to be fixed, because AnimationPresence is not working properly with createBrowserRouter

--- barber-web/src/components/transition.tsx
+++ barber-web/src/components/transition.tsx
@@ -5,12 +5,20 @@ export default (SingleComponent : ComponentType) => {
     return (props: any) => (
         <>
             <SingleComponent />
-            <motion.div className="fixed top-0 left-0 w-full h-screen origin-bottom bg-white z-[60]" initial={{ scaleY: 0 }}
-            animate={{ scaleY: 0 }} exit={{ scaleY: 1 }}
-            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }} />
-            <motion.div className="fixed top-0 left-0 w-full h-screen origin-top bg-charcoal z-[70]" initial={{ scaleY: 1 }}
-            animate={{ scaleY: 0 }} exit={{ scaleY: 0 }}
-            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }} />
+            <motion.div
+                className="fixed top-0 left-0 w-full h-screen origin-right bg-charcoal z-[60]"
+                initial={{ scaleX: 0 }}
+                animate={{ scaleX: 0 }}
+                exit={{ scaleX: 1 }}
+                transition={{ duration: 2, ease: [0.22, 1, 0.36, 1] }}
+            />
+            <motion.div
+                className="fixed top-0 left-0 w-full h-screen origin-left bg-charcoal z-[60]"
+                initial={{ scaleX: 1 }}
+                animate={{ scaleX: 0 }}
+                exit={{ scaleX: 0 }}
+                transition={{ duration: 2, ease: [0.22, 1, 0.36, 1] }}
+            />
         </>
     );
 }
