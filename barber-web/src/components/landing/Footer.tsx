8d08b3ca4640e83e3a9e6b02dbf1ff24e02ecf74
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-01-19 03:32:08 +0100

    Hashlink navigation

--- barber-web/src/components/landing/Footer.tsx
+++ barber-web/src/components/landing/Footer.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
 import SmallAnchor from "./SmallAnchor";
 
 export default () => {
@@ -7,9 +9,15 @@ export default () => {
         <footer className="relative w-full h-auto bg-white text-charcoal font-lato flex flex-row items-center justify-center py-16 px-8 sm:px-16 z-10">
             <h3 className="font-cormorant font-medium text-xl py-16 px-8 sm:px-16">MSTUDIO</h3>
             <div className="flex flex-col justify-center py-16 px-8 sm:px-16">
-                <SmallAnchor>Wstęp</SmallAnchor>
-                <SmallAnchor>Poznaj nas</SmallAnchor>
-                <SmallAnchor>Oferta</SmallAnchor>
+                <HashLink smooth to="#wstep">
+                    <SmallAnchor>Wstęp</SmallAnchor>
+                </HashLink>
+                <HashLink smooth to="#poznaj-nas">
+                    <SmallAnchor>Poznaj nas</SmallAnchor>
+                </HashLink>
+                <HashLink smooth to="#oferty">
+                    <SmallAnchor>Oferta</SmallAnchor>
+                </HashLink>                
                 <Link to='/rezerwacja'>
                     <SmallAnchor>Rezerwacja</SmallAnchor>
                 </Link>   
