9ade76655e208ab54285c5981908207cf8b666c3
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-02-06 02:43:29 +0100

    Mobile basket button with offers count and animation

--- barber-web/src/components/reservation/nav/BasketButton.tsx
+++ barber-web/src/components/reservation/nav/BasketButton.tsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "motion/react";
+import { useAppSelector } from "../../../store/hooks";
+import { ShoppingBasket } from "lucide-react";
+
 export default () => {
+    const selectedItems = useAppSelector((state) => state.cart.items);
+    const totalCount = selectedItems.reduce((sum, offer) => sum + offer.quantity, 0);
+    const controls = useAnimation();
+
+    useEffect(() => {
+        if (totalCount > 0) {
+            controls.start({
+                scale: [1, 1.2, 1],
+                transition: { duration: 0.3 }
+            });
+        }
+    }, [totalCount, controls]);
+
     return (
-        <button className=""></button>
+        <button className="relative md:hidden ml-4 outline-none">
+            <ShoppingBasket color="#353535" size={24} strokeWidth={1.25} />
+            <motion.div 
+                className="select-none absolute -top-1 -right-1 w-5 h-5 bg-charcoal text-white text-xs font-extrabold rounded-full flex items-center justify-center"
+                animate={controls}
+            >
+                {totalCount}
+            </motion.div>
+        </button>
     );
 }
--- barber-web/src/components/reservation/nav/NavBar.tsx
+++ barber-web/src/components/reservation/nav/NavBar.tsx
@@ -2,13 +2,15 @@ import { NavLink } from "react-router-dom";
 
 import NavButton from "./NavButton";
 import UserInfo from "./UserInfo";
+import BasketButton from "./BasketButton";
 
 export default () => {
     return (
         <div className='w-full sticky border-stone-300 border-b md:border-none bg-white md:bg-stone-100 top-0 left-0 md:w-[46rem] h-auto py-2 md:py-4 flex flex-row items-center justify-between md:relative overflow-hidden z-40'>
-            
-            
-            <UserInfo />
+            <div className="flex flex-row items-center">
+                <BasketButton />
+                <UserInfo />
+            </div>
             <div className="flex flex-row items-center">
                 <NavLink to="/rezerwacja/moje-rezerwacje">
                     {({ isActive }) => (
